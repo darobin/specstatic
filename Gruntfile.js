daba37ac6439a740af6e4cf707af946ca6d63032
Robin Berjon <robin@berjon.com>
2013-10-31 17:09:32 +0100

    anchors to headings ok

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,19 +1,39 @@
 
 module.exports = function (grunt) {
+    var banner = "/*! <%= pkg.name %>/<%= pkg.version%>, <%= grunt.template.today('yyyy-mm-dd') %>\n" +
+                 "    Robin Berjon <robin@berjon.com>, http://berjon.com/, @robinberjon */\n"
+    ;
     grunt.initConfig({
         pkg:    grunt.file.readJSON("package.json")
     ,   uglify: {
             options: {
-                banner: "/*! <%= pkg.name %>/<%= pkg.version%>, <%= grunt.template.today('yyyy-mm-dd') %>\n" +
-                        "    Robin Berjon <robin@berjon.com>, http://berjon.com/, @robinberjon */\n"
+                banner: banner
             }
         ,   build: {
                 src:    "specstatic.js"
             ,   dest:   "specstatic.min.js"
             }
         }
+    ,   cssmin: {
+            minify: {
+                removeEmpty:    true
+            ,   src:            "specstatic.css"
+            ,   dest:           "specstatic.min.css"
+            }
+        }
+    ,   concat: {
+            options: {
+                banner: banner
+            }
+        ,   dist: {
+                src:    ["specstatic.min.css"]
+            ,   dest:   "specstatic.min.css"
+            }
+        }
     });
 
     grunt.loadNpmTasks("grunt-contrib-uglify");
-    grunt.registerTask("default", ["uglify"]);
+    grunt.loadNpmTasks("grunt-contrib-cssmin");
+    grunt.loadNpmTasks("grunt-contrib-concat");
+    grunt.registerTask("default", ["uglify", "cssmin", "concat"]);
 };
--- specstatic.js
+++ specstatic.js
@@ -3,10 +3,66 @@
 (function () {
     // no point in responding to hopeless browsers
     if (!("querySelector" in document)) return;
+    
+    var headerLink
+    ;
+
+    // tooltip management
+    function toolTip (options) {
+        if (!options.content || !options.target) return;
+        var el = document.createElement("div");
+        el.classList.add("specstatic-tooltip");
+        el.innerHTML = options.content;
+        if (options.position === "append") {
+            el.classList.add("specstatic-append");
+            options.target.appendChild(el);
+        }
+        return el;
+    }
+    
+    function clearToolTip (el) {
+        el.parentNode.removeChild(el);
+    }
 
     // the meat of the wonder
     function specstatic () {
-        console.log("I am specstatic!");
+        // find ourselves and inject CSS to match
+        var src = document.currentScript ? document.currentScript.src : null;
+        if (!src) {
+            var scripts = document.querySelectorAll("script[src]");
+            for (var i = 0, n = scripts.length; i < n; i++) {
+                var s = scripts[i]
+                ,   tmpSrc = s.getAttribute("src")
+                ;
+                if (!/specstatic(\.min)?\.js$/.test(tmpSrc)) continue;
+                src = tmpSrc;
+                break;
+            }
+        }
+        src = src.replace(/\.js$/, ".css");
+        var lnk = document.createElement("link");
+        lnk.setAttribute("href", src);
+        lnk.setAttribute("rel", "stylesheet");
+        document.head.appendChild(lnk);
+        
+        // enhance anchors
+        var ids = document.querySelectorAll("[id]");
+        for (var i = 0, n = ids.length; i < n; i++) {
+            var el = ids[i];
+            if (/^h[2-6]$/i.test(el.tagName)) {
+                el.addEventListener("mouseenter", function (ev) {
+                    var id = ev.target.getAttribute("id");
+                    headerLink = toolTip({
+                        target:     ev.target
+                    ,   position:   "append"
+                    ,   content:    "<a href='#" + id + "' class='specstatic-anchorlink'>§</a>"
+                    });
+                });
+                el.addEventListener("mouseleave", function () {
+                    clearToolTip(headerLink);
+                });
+            }
+        }
     }
     
     // trigger load either after ReSpec if present, or when we have the DOM
