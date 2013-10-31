947f2f3465f472f490a5f6feb37a0505d1e36bbd
Robin Berjon <robin@berjon.com>
2013-10-31 15:50:43 +0100

    load logic

--- specstatic.js
+++ specstatic.js
@@ -1,6 +1,20 @@
+/*global respecEvents */
 
 (function () {
+    // no point in responding to hopeless browsers
+    if (!("querySelector" in document)) return;
+
+    // the meat of the wonder
+    function specstatic () {
+        console.log("I am specstatic!");
+    }
     
+    // trigger load either after ReSpec if present, or when we have the DOM
+    if ("respecEvents" in window) {
+        if (document.respecDone) return specstatic();
+        respecEvents.sub("end-all", specstatic);
+    }
+    else {
+        document.addEventListener("DOMContentLoaded", specstatic);
+    }
 }());
-
-
